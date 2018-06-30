b95d0ce6fc26d3e465cfd66d9cdbc87f208e9d39
Paul Jackob <pauljackom911@hotmail.com>
2018-06-30 11:25:21 +0200

    added homework more java

---
+++ my2java.js
@@ -0,0 +1,95 @@
+console.log("more java");
+
+//more javascript
+//1
+function f(a, b, c) {
+    return a + b + c;
+  }
+
+ console.log( f(27, 72, 22) );
+
+ //2
+ function colorCar(color){
+     
+     console.log('a', color , 'car');
+ }
+
+colorCar("red");
+
+
+ //3
+ function func1(object) {
+    return object = {
+        Name:"Sam",
+        Age:"13"
+    }
+   
+  }
+
+  console.log(func1());
+
+//4
+//   function colorType( color, type) {
+//       var type =['car', 'motorbike'];
+//       console.log('a', color, type.element[index]);
+
+//   }
+//  colorType('blue',[0]);
+
+//5
+console.log(3===3 ? "yes" : "no");
+
+//6
+
+//7
+let vehicles = ["motorbike", "caravan", "bike", "vessel"];
+function vehicle(){
+    console.log(vehicles[2]);
+}
+vehicle();
+
+//8
+
+//9
+
+//10
+
+
+//11
+
+
+//12
+let empty = {};
+
+//13 - 14
+  var teachers = {
+    Nick:"HTML + CSS",
+    Tiago:"command line",
+    Claudio:"GIT",
+    Rob:"javascript"
+};
+
+console.log(teachers);
+
+//15
+let x = [1,2,3];
+let y = [1,2,3];
+let z = y;
+console.log( x == y );
+console.log( x === y );
+console.log( z == y );
+console.log( z == x );
+
+//16
+// let o1 = { foo: 'bar' };
+// let o2 = { foo: 'bar' };
+// let o3 = o2;
+// console.log( o1 == o2);
+// console.log( o2 == o3);
+// console.log( o1 == o3);
+
+
+//17
+// It will return 'string' because typeof ( typeof anything ) is always string.
+
+
